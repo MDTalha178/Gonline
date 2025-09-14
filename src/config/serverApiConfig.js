252fe569b587d61a3c1465ff37825d8e7ec23edc
mdtalha178 <talhaazmi178@gmail.com>
2025-09-15 02:49:43 +0530

    responsive and shimmer effects

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -3,19 +3,19 @@
 console.log('Store Service URL:',  import.meta.env.VITE_ADMIN_SERVICE_URL);
 
 const SERVICE_CONFIGS = {
-    USER_SERVICE: {
-        baseURL:import.meta.env.VITE_USER_SERVICE_URL,
-        timeout: 3000,
-    },
-    STORE_SERVICE: {
-        baseURL: import.meta.env.VITE_STORE_SERVICE_URL,
-        timeout: 10000, 
-    },
-    ADMIN_SERVICE: {
-        baseURL: import.meta.env.VITE_ADMIN_SERVICE_URL,
-        timeout: 10000, 
-    }
-    
+  USER_SERVICE: {
+    baseURL: import.meta.env.VITE_USER_SERVICE_URL,
+    timeout: 10000, // 10 seconds
+  },
+  STORE_SERVICE: {
+    baseURL: import.meta.env.VITE_STORE_SERVICE_URL,
+    timeout: 10000, 
+  },
+  ADMIN_SERVICE: {
+    baseURL: import.meta.env.VITE_ADMIN_SERVICE_URL,
+    timeout: 10000, 
+  }
 };
+
 
 export default SERVICE_CONFIGS
