dcb2de6ce43d2be806d58a1a7a5ee4efbfdf98b0
mdtalha178 <talhaazmi178@gmail.com>
2025-09-15 13:46:21 +0530

    catgeiry issue

--- src/config/serverApiConfig.js
+++ src/config/serverApiConfig.js
@@ -5,15 +5,15 @@ console.log('Store Service URL:',  import.meta.env.VITE_ADMIN_SERVICE_URL);
 const SERVICE_CONFIGS = {
   USER_SERVICE: {
     baseURL: import.meta.env.VITE_USER_SERVICE_URL,
-    timeout: 10000, // 10 seconds
+    timeout: 60000, // 60 seconds
   },
   STORE_SERVICE: {
     baseURL: import.meta.env.VITE_STORE_SERVICE_URL,
-    timeout: 10000, 
+    timeout: 60000, // 60 seconds
   },
   ADMIN_SERVICE: {
     baseURL: import.meta.env.VITE_ADMIN_SERVICE_URL,
-    timeout: 10000, 
+    timeout: 60000, // 60 seconds
   }
 };
 
